2516c7e523992f19c2d69a13203c271eecb9bc9b
Petter55 <e.petter@hotmail.com>
2019-12-02 00:36:02 -0300

    drop shadow on logo; new text font

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Row style={{paddingLeft:"50px", paddingRight:"50px"}}>
         <Col  sm={{ size: 10, offset: 1 }} md={{ size: 6, offset: 3 }}>
           <p style={{textAlign:"center"}}>
-          Associação de defesa dos direitos dos usuários de serviços de saúde mental - Porto Alegre, RS.
+          <i>Associação de defesa dos direitos dos usuários de serviços de saúde mental - Porto Alegre, RS.</i>
           </p>
         </Col>
       
@@ -18,14 +18,15 @@ function App() {
       <Row style={{paddingLeft:"50px", paddingRight:"50px"}}>
       <Col  sm={{ size: 10, offset: 1 }} md={{ size: 6, offset: 3 }}>
         <p style={{textAlign:"center"}}>
-         Impressão sublimática em Tecido, canecas, azulejos etc.
+        <i>Impressão sublimática em Tecido, canecas, azulejos etc.</i>
         </p>
         </Col>
         </Row>
       <Row style={{paddingLeft:"50px", paddingRight:"50px"}}>
         <Col  sm={{ size: 10, offset: 1 }} md={{ size: 6, offset: 3 }}>
           <p style={{textAlign:"center"}}>
-          Reuniões fixas todas as segundas-feiras das 14h às 16:30h, aberta ao publico, porém é necessário pedir que um membro abra a porta do prédio para acesso, pois não tem porteiro/portaria no local.
+          <i>Reuniões fixas todas as segundas-feiras das 14h às 16:30h, aberta ao publico, porém é necessário pedir que um membro abra a porta do prédio para acesso, pois não tem porteiro/portaria no local.
+          </i>
           </p>
         </Col>
       </Row>
--- src/header/Header.js
+++ src/header/Header.js
@@ -4,7 +4,7 @@ let imgs = require('../logoAssocia.png')
 
 function Header(){
     return (<header>
-        <div id="imageContainer" style={{padding:"5px", paddingLeft:"15px", boxShadow: "1px 1px 5px #999999 !important", height:"90%", width: "auto", float:"left", borderRadius: "100px"}}>
+        <div id="imageContainer" style={{margin:"5px", marginLeft:"15px", marginRight:"15px", height:"90%", width: "auto", float:"left", borderRadius: "100px"}}>
             <img src={imgs} style={{boxShadow: "1px 1px 5px #888888 !important", maxWidth:"100%",maxHeight:"100%"}}/>
         </div>
 
