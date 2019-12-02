436c37e29048c8f5c2058ab83aebcf15d80abc72
Petter55 <e.petter@hotmail.com>
2019-12-02 01:42:44 -0300

    moved fonts and images to folders; added social media icons

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './Header.css';
-let imgs = require('../logoAssocia.png')
+const logoImg = require('../images/logoAssocia.png')
+const facebook = require('../images/facelogo.png')
+const instagram = require('../images/insta2.png')
+const email = require('../images/email2.png')
 
 function Header(){
     return (<header>
         <div id="imageContainer" style={{margin:"5px", marginLeft:"15px", marginRight:"15px", height:"90%", width: "auto", float:"left", borderRadius: "100px"}}>
-            <img src={imgs} style={{boxShadow: "1px 1px 5px #888888 !important", maxWidth:"100%",maxHeight:"100%"}}/>
+            <img src={logoImg} style={{boxShadow: "1px 1px 5px #888888 !important", maxWidth:"100%",maxHeight:"100%"}}/>
         </div>
+        <div style={{position: "relative", top: "50%", height:"30px", width: "auto",float:"left", margin: "5px"}}>
+        <a href="https://www.facebook.com/associacaoconstrucao/"><img src={facebook} style={{maxWidth:"100%",maxHeight:"100%"}}/></a>
+        </div>
+        <div style={{position: "relative", top: "50%", height:"30px", width: "auto", float:"left", margin: "5px"}}>
+        <a href="https://instagram.com/associacaoconstrucao?igshid=1lceiijed8ebq"><img src={instagram} style={{maxWidth:"100%",maxHeight:"100%"}}/></a>
+        </div>
+        <div style={{position: "relative", top: "50%", height:"30px", width: "auto", float:"left", margin: "5px"}}>
+        <a href="mailto: associacaoconstrucao@gmail.com"><img src={email} style={{maxWidth:"100%",maxHeight:"100%"}}/></a>
+        
+        </div>
+        
 
     </header>)
 }
