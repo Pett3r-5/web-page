e2f98d670ff65b84b1fc3d8859831809003a06a0
Petter55 <e.petter@hotmail.com>
2019-12-01 23:48:36 -0300

    formatting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import './App.css';
 import Header from './header/Header';
+import { Col, Container, Button, Row } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <p>
-      Associação de defesa dos direitos dos usuários de serviços de saúde mental - Porto Alegre, RS.
-      </p>
-      <p>
-      Impressão sublimática em Tecido, canecas, azulejos etc.
-      </p>
-      <p>
-      Reuniões fixas todas as segundas-feiras das 14h às 16:30h, aberta ao publico, porém é necessário pedir que um membro abra a porta do prédio para acesso, pois não tem porteiro/portaria no local.
-      </p>
+      <Row style={{paddingLeft:"50px", paddingRight:"50px"}}>
+        <Col  sm={{ size: 10, offset: 1 }} md={{ size: 6, offset: 3 }}>
+          <p style={{textAlign:"center"}}>
+          Associação de defesa dos direitos dos usuários de serviços de saúde mental - Porto Alegre, RS.
+          </p>
+        </Col>
+      
+      </Row>
+      <Row style={{paddingLeft:"50px", paddingRight:"50px"}}>
+      <Col  sm={{ size: 10, offset: 1 }} md={{ size: 6, offset: 3 }}>
+        <p style={{textAlign:"center"}}>
+         Impressão sublimática em Tecido, canecas, azulejos etc.
+        </p>
+        </Col>
+        </Row>
+      <Row style={{paddingLeft:"50px", paddingRight:"50px"}}>
+        <Col style={{paddingLeft:"15px", paddingRight:"15px"}}>
+          <p style={{textAlign:"center"}}>
+          Reuniões fixas todas as segundas-feiras das 14h às 16:30h, aberta ao publico, porém é necessário pedir que um membro abra a porta do prédio para acesso, pois não tem porteiro/portaria no local.
+          </p>
+        </Col>
+      </Row>
+      
+      
     </div>
   );
 }
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Header.css';
-
+let imgs = require('../logoAssocia.png')
 
 function Header(){
     return (<header>
-        <img style={{textAlign:"left", boxShadow: "1px 1px 5px #888888 !important", height:"150px"}} src={require('../logoAssocia.png')} />
+        <div id="imageContainer" style={{padding:"5px", paddingLeft:"15px", boxShadow: "1px 1px 5px #999999 !important", height:"90%", width: "auto", float:"left", borderRadius: "100px"}}>
+            <img src={imgs} style={{boxShadow: "1px 1px 5px #888888 !important", maxWidth:"100%",maxHeight:"100%"}}/>
+        </div>
+
     </header>)
 }
 
